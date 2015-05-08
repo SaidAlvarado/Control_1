0b58a9b156c477dcd53cfc872b8284d5d8897ab9
Said Alvarado <said_alvarado@hotmail.com>
2015-05-07 22:30:02 -0430

    La parte 2 del pre1 fue agregada, y se ardenaron las imagenes en un word

--- Practica 1/Prelaboratorio/Asignacion1_grupo8.m
+++ Practica 1/Prelaboratorio/Asignacion1_grupo8.m
@@ -23,15 +23,18 @@ figure; plot(t,y1); title('Y1(t) = Sin(t)'); xlabel('t'); ylabel('Y1(t)');grid()
 figure; plot(t,y2); title('Y2(t) = Sin(8t) + Sin(t)'); xlabel('t'); ylabel('Y2(t)');grid(); %axis([-0.001 0.001 -10 10]) ;
 figure; plot(t,y3); title('Y3(t) = Sin(50t) + Sin(t)'); xlabel('t'); ylabel('Y3(t)');grid(); %axis([-0.001 0.001 -10 10]) ;
 % 7)
-figure; 
+figure;
 subplot(3,1,1); plot(t,y1); title('Y1(t) = Sin(t)'); xlabel('t'); ylabel('Y1(t)');grid(); %axis([-0.001 0.001 -10 10]) ;
 subplot(3,1,2); plot(t,y2); title('Y2(t) = Sin(8t) + Sin(t)'); xlabel('t'); ylabel('Y2(t)');grid(); %axis([-0.001 0.001 -10 10]) ;
 subplot(3,1,3); plot(t,y3); title('Y3(t) = Sin(50t) + Sin(t)'); xlabel('t'); ylabel('Y3(t)');grid(); %axis([-0.001 0.001 -10 10]) ;
 % 8)
-figure; 
+figure;
 subplot(3,1,1); plot(t,y1,'r'); title('Y1(t) = Sin(t)'); xlabel('t'); ylabel('Y1(t)');grid(); %axis([-0.001 0.001 -10 10]) ;
 subplot(3,1,2); plot(t,y2,'g'); title('Y2(t) = Sin(8t) + Sin(t)'); xlabel('t'); ylabel('Y2(t)');grid(); %axis([-0.001 0.001 -10 10]) ;
 subplot(3,1,3); plot(t,y3,'y'); title('Y3(t) = Sin(50t) + Sin(t)'); xlabel('t'); ylabel('Y3(t)');grid(); %axis([-0.001 0.001 -10 10]) ;
 % 9)
 error_y1_y2 = abs(y1 - y2)
 error_y1_y3 = abs(y1 - y3)
+
+
+%uiwait()
